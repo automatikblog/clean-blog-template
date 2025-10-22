13dd353a11e2669ab971c8d2d103d696d5bd2e35
automatikblog <automatiklabs13@gmail.com>
2025-10-22 13:35:35 -0300

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://example.com",
   adapter: vercel(),
   integrations: [mdx(), sitemap()],
 });
