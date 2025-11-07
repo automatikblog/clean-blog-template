b76eaf1a4f06d97a24673142b26df4e330178280
automatikblog <automatiklabs13@gmail.com>
2025-11-07 15:32:23 -0300

    sa

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,32 @@
-import { getCollection } from 'astro:content';
-import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import { getCollection } from "astro:content";
+import rss from "@astrojs/rss";
+import { SITE_TITLE, SITE_DESCRIPTION, SITE_LANGUAGE } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  const posts = await getCollection("blog");
+
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description || post.data.metaDescription,
+      link: `/blog/${post.id}/`,
+      pubDate: post.data.pubDate,
+      // Optional media tag for Google Discover / News
+      customData: post.data.heroImage
+        ? `<media:content url="${post.data.heroImage}" medium="image" />`
+        : "",
+    })),
+    customData: `
+      <language>${SITE_LANGUAGE}</language>
+      <image>
+        <url>${context.site}favicon.png</url>
+        <title>${SITE_TITLE}</title>
+        <link>${context.site}</link>
+      </image>
+      <atom:link href="${context.site}rss.xml" rel="self" type="application/rss+xml" />
+    `,
+  });
 }
